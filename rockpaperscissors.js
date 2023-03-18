114057592ad9ddde00691e6c1583617561f39bf7
Emily <egp10990@yahoo.com>
2023-03-18 18:05:30 -0500

    Add second js function for player choice on rockpaperscissors

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,3 +2,9 @@ function getComputerChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }    
 let computerSelection = getComputerChoice(["Rock", "Paper", "Scissors"]);
+
+function getPlayerChoice(playerInput) {
+    return playerInput.toLowerCase();
+}
+let playerInput = prompt("Rock, paper, or scissors?");
+    
