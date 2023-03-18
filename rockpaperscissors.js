def7bad50ec03f87e4729120b37a609baaa19500
Emily <egp10990@yahoo.com>
2023-03-17 19:35:41 -0500

    Write js function for rockpaperscissors game scoring

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -8,5 +8,35 @@ function getPlayerChoice() {
     let result = playerInput.toLowerCase();
     return result;
 }
-
-console.log()
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "You tied.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose. Paper covers Rock.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win. Rock crushes Scissors."
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win. Paper covers Rock."
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose. Scissors cuts Paper."
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose. Rock crushes Scissors."
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win. Scissors cuts Paper."
+    }
+}
+function game() {
+    let computerScore = 0,
+    playerScore = 0;
+    for (i = 0; i < 3; i ++) {
+        const result = playRound(getPlayerChoice(), getComputerChoice());
+        console.log(result);
+        if(result.includes("Win")) {
+        playerScore ++;
+        console.log(`Computer: ${computerScore} | Player: ${playerScore}`);
+    } else if (result.includes("Lose")) {
+        computerScore ++;
+        console.log(`Computer: ${computerScore} | Player: ${playerScore}`)
+    }
+    } 
+}
