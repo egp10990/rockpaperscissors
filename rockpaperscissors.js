8e989c19bb4ca362953ea1a92941ca6e8943d4c0
Emily <egp10990@yahoo.com>
2023-03-18 18:15:54 -0500

    Add function for game rules

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,8 +3,25 @@ function getComputerChoice(choices) {
 }    
 let computerSelection = getComputerChoice(["Rock", "Paper", "Scissors"]);
 
-function getPlayerChoice(playerInput) {
-    return playerInput.toLowerCase();
+function getPlayerChoice(playerSelection) {
+    return playerSelection.toLowerCase();
 }
-let playerInput = prompt("Rock, paper, or scissors?");
-    
+let playerSelection = prompt("Rock, paper, or scissors?");
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "You tied.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose. Paper covers Rock.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win. Rock crushes Scissors."
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win. Paper covers Rock."
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose. Scissors cuts Paper."
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose. Rock crushes Scissors."
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win. Scissors cuts Paper."
+    }
+}
