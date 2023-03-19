622141ae5e4a2d26b293d813b81b959078113b2f
Emily <egp10990@yahoo.com>
2023-03-19 17:47:16 -0500

    Redo game rule function and add let parameters

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"]
-    let randomChoice = Math.floor(Math.random()* choices.length) + 1
+    let computerChoices = ["Rock", "Paper", "Scissors"]
+    let randomChoice = Math.floor(Math.random()* computerChoices.length) + 1
     if (randomChoice === 1){
         return "Rock"
     } if (randomChoice === 2) {
@@ -9,5 +9,26 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
+let playerSelection = prompt("Rock, paper, or scissors?");
+let computerSelection = getComputerChoice();
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "You tied.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose. Paper covers Rock.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win. Rock crushes Scissors."
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win. Paper covers Rock."
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose. Scissors cuts Paper."
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose. Rock crushes Scissors."
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win. Scissors cuts Paper."
+    }
+}
+
+console.log("The computer chose: " + computerSelection);
 
