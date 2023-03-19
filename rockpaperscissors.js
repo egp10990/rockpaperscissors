071b6c931bfc4fc82aec163fbeb62e78fc82c850
Emily <egp10990@yahoo.com>
2023-03-19 16:45:16 -0500

    Redo computer choice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,27 +1,13 @@
-function getComputerChoice(choices) {
-    return choices[Math.floor(Math.random() * choices.length)];
-}    
-let computerSelection = getComputerChoice(["Rock", "Paper", "Scissors"]);
-
-function getPlayerChoice(playerSelection) {
-    return playerSelection.toLowerCase();
-}
-let playerSelection = prompt("Rock, paper, or scissors?");
-
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "You tied.";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose. Paper covers Rock.";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win. Rock crushes Scissors."
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win. Paper covers Rock."
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose. Scissors cuts Paper."
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose. Rock crushes Scissors."
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win. Scissors cuts Paper."
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"]
+    let randomChoice = Math.floor(Math.random()* choices.length) + 1
+    if (randomChoice === 1){
+        return "Rock"
+    } if (randomChoice === 2) {
+        return "Paper"
+    } if (randomChoice === 3) {
+        return "Scissors"
     }
 }
+
+
